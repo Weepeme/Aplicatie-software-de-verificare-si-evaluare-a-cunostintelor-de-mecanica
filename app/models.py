710a15757827b7c0604bd7a95976c92a67042c68
Weepeme <aura.adrian100@yahoo.com>
2025-06-16 11:54:49 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from flask_login import UserMixin
+from flask_login import UserMixin  # Import pentru clasa UserMixin, care ajută la integrarea cu Flask-Login
 
 class User(UserMixin):
+    # Constructorul clasei User primește un dicționar (user_dict) ce conține datele utilizatorului din MongoDB
     def __init__(self, user_dict):
-        self.id = str(user_dict.get('_id'))
-        self.username = user_dict.get('username')
-        self.role = user_dict.get('role')
+        self.id = str(user_dict.get('_id'))  # ID-ul utilizatorului, convertit în string (necesar pentru Flask-Login)
+        self.username = user_dict.get('username')  # Numele de utilizator
+        self.role = user_dict.get('role')  # Rolul utilizatorului (ex: 'profesor')
